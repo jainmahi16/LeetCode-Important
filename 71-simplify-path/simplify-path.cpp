4321fd133d004a7fc3a2a746afcf23f5c08296e6
Mahi Jain <mahijain2022@vitbhopal.ac.in>
2025-07-26 23:51:17 +0530

    Time: 4 ms (43.24%) | Memory: 11.1 MB (68.41%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,47 +1,16 @@
-					// \U0001f609\U0001f609\U0001f609\U0001f609Please upvote if it helps \U0001f609\U0001f609\U0001f609\U0001f609
 class Solution {
 public:
     string simplifyPath(string path) {
+        string res, tmp;
+    vector<string> stk;
+    stringstream ss(path);
+    while(getline(ss,tmp,'/')) {
+        if (tmp == "" or tmp == ".") continue;
+        if (tmp == ".." and !stk.empty()) stk.pop_back();
+        else if (tmp != "..") stk.push_back(tmp);
+    }
+    for(auto str : stk) res += "/"+str;
+    return res.empty() ? "/" : res;
         
-        stack<string> st;
-        string res;
-        
-        for(int i = 0;  i<path.size(); ++i)
-        {
-            if(path[i] == '/')    
-                continue;
-            string temp;
-			// iterate till we doesn't traverse the whole string and doesn't encounter the last /
-            while(i < path.size() && path[i] != '/')
-            {
-				// add path to temp string
-                temp += path[i];
-                ++i;
-            }
-            if(temp == ".")
-                continue;
-			// pop the top element from stack if exists
-            else if(temp == "..")
-            {
-                if(!st.empty())
-                    st.pop();
-            }
-            else
-			// push the directory file name to stack
-                st.push(temp);
-        }
-        
-		// adding all the stack elements to res
-        while(!st.empty())
-        {
-            res = "/" + st.top() + res;
-            st.pop();
-        }
-        
-		// if no directory or file is present
-        if(res.size() == 0)
-            return "/";
-        
-        return res;
     }
 };
