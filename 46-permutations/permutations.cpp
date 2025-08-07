5abba277b95828e8e16225b6f10c494cd1bdac4a
Mahi Jain <mahijain2022@vitbhopal.ac.in>
2025-08-07 23:59:51 +0530

    Time: 4 ms (12.88%) | Memory: 10.6 MB (47.04%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,22 +1,55 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    
-    void backtrack(vector<int>& nums, int start) {
-        if (start == nums.size()) {
-            res.push_back(nums);
-            return;
+    vector<int> nextGreaterPermutation(vector<int>& A) {
+        int n = A.size();
+        int ind = -1;
+
+        // Step 1: Find pivot
+        for (int i = n - 2; i >= 0; --i) {
+            if (A[i] < A[i + 1]) {
+                ind = i;
+                break;
+            }
         }
-        
-        for (int i = start; i < nums.size(); i++) {
-            swap(nums[start], nums[i]);  // Choose
-            backtrack(nums, start + 1);  // Explore
-            swap(nums[start], nums[i]);  // Un-choose (backtrack)
+
+        // If no pivot found, array is in descending order
+        if (ind == -1) {
+            reverse(A.begin(), A.end());
+            return A;
         }
+
+        // Step 2: Find element just larger than pivot and swap
+        for (int j = n - 1; j > ind; --j) {
+            if (A[j] > A[ind]) {
+                swap(A[j], A[ind]);
+                break;
+            }
+        }
+
+        // Step 3: Reverse the suffix
+        reverse(A.begin() + ind + 1, A.end());
+
+        return A;
     }
-    
+
     vector<vector<int>> permute(vector<int>& nums) {
-        backtrack(nums, 0);
-        return res;
+        vector<vector<int>> ans;
+
+        sort(nums.begin(), nums.end()); // Start with the smallest permutation
+        ans.push_back(nums);
+
+        // Compute total number of permutations: n!
+        int total = 1;
+        for (int i = 2; i <= nums.size(); ++i) {
+            total *= i;
+        }
+
+        // Generate remaining permutations using nextGreaterPermutation
+        for (int i = 1; i < total; ++i) {
+            nums = nextGreaterPermutation(nums);
+            ans.push_back(nums);
+        }
+
+        return ans;
     }
 };
